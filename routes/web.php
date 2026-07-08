083162abe7a0d38f381b7e4ec6376592b6d16e05
ryuzen16 <riskysaputrad@gmail.com>
2026-07-08 08:04:44 +0700

    Menambahkan sistem login dan dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\GuruController;
 
-Route::redirect('/', '/dashboard');
+Route::redirect('/', '/login');
 
-Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::middleware('auth')->group(function () {
 
-Route::resource('guru', GuruController::class);
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
+
+    Route::resource('guru', GuruController::class);
+
+});
 
 require __DIR__.'/auth.php';
