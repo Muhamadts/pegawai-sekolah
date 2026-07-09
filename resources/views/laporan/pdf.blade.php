dbe6f4f739424128c438b0ad8261f36a16117b75
ryuzen16 <riskysaputrad@gmail.com>
2026-07-09 23:15:12 +0700

    feat: implement laporan dan fitur cetak PDF

---
+++ resources/views/laporan/pdf.blade.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+
+<meta charset="UTF-8">
+
+<title>Laporan</title>
+
+<style>
+
+body{
+
+    font-family: DejaVu Sans, sans-serif;
+    font-size:12px;
+    color:#000;
+
+}
+
+.header{
+
+    text-align:center;
+    margin-bottom:15px;
+
+}
+
+.header h2{
+
+    margin:0;
+    font-size:22px;
+
+}
+
+.header h4{
+
+    margin:5px 0;
+
+}
+
+.header p{
+
+    margin:2px 0;
+
+}
+
+.line{
+
+    border-top:3px solid black;
+    border-bottom:1px solid black;
+    margin:15px 0;
+
+}
+
+table{
+
+    width:100%;
+    border-collapse:collapse;
+
+}
+
+table th{
+
+    background:#eeeeee;
+    border:1px solid #000;
+    padding:8px;
+    text-align:center;
+
+}
+
+table td{
+
+    border:1px solid #000;
+    padding:7px;
+
+}
+
+.text-center{
+
+    text-align:center;
+
+}
+
+.footer{
+
+    margin-top:50px;
+    width:100%;
+
+}
+
+.signature{
+
+    float:right;
+    width:250px;
+    text-align:center;
+
+}
+
+</style>
+
+</head>
+
+<body>
+
+<div class="header">
+
+    <h2>
+
+        SD PLUS IGM PALEMBANG
+
+    </h2>
+
+    <p>
+
+        Jl. Pendidikan No.1 Palembang
+
+    </p>
+
+    <p>
+
+        Telp. (0711) 123456
+
+    </p>
+
+    <p>
+
+        Email : info@sdplusigm.sch.id
+
+    </p>
+
+</div>
+
+<div class="line"></div>
+
+<div class="text-center">
+
+@if($jenis=='guru')
+
+<h3>
+
+LAPORAN DATA GURU
+
+</h3>
+
+@elseif($jenis=='tendik')
+
+<h3>
+
+LAPORAN DATA TENDIK
+
+</h3>
+
+@elseif($jenis=='data_induk')
+
+<h3>
+
+LAPORAN DATA INDUK
+
+</h3>
+
+@endif
+
+<p>
+
+Tanggal Cetak :
+{{ date('d-m-Y') }}
+
+</p>
+
+</div>
+
+<table>
+
+<thead>
+
+<tr>
+
+<th width="5%">
+
+No
+
+</th>
+
+<th width="12%">
+
+NIY
+
+</th>
+
+<th width="20%">
+
+Nama
+
+</th>
+
+<th width="22%">
+
+Tempat / Tanggal Lahir
+
+</th>
+
+<th width="10%">
+
+JK
+
+</th>
+
+<th width="16%">
+
+Jabatan
+
+</th>
+
+<th width="15%">
+
+Pendidikan
+
+</th>
+
+</tr>
+
+</thead>
+
+<tbody>
+
+@forelse($laporan as $item)
+
+<tr>
+
+<td class="text-center">
+
+{{ $loop->iteration }}
+
+</td>
+
+<td>
+
+{{ $item->niy }}
+
+</td>
+
+<td>
+
+{{ $item->nama }}
+
+</td>
+
+<td>
+
+{{ $item->tempat_lahir }},
+{{ \Carbon\Carbon::parse($item->tanggal_lahir)->format('d-m-Y') }}
+
+</td>
+
+<td class="text-center">
+
+{{ $item->jenis_kelamin }}
+
+</td>
+
+<td>
+
+{{ $item->jabatan }}
+
+</td>
+
+<td>
+
+{{ $item->pendidikan }}
+
+</td>
+
+</tr>
+
+@empty
+
+<tr>
+
+<td colspan="7" class="text-center">
+
+Data tidak tersedia
+
+</td>
+
+</tr>
+
+@endforelse
+
+</tbody>
+
+</table>
+
+<div class="footer">
+
+<div class="signature">
+
+<p>
+
+Palembang,
+
+{{ date('d F Y') }}
+
+</p>
+
+<br><br><br>
+
+<p>
+
+<b>
+
+Mefi Laranti, S.Pd
+
+</b>
+
+</p>
+
+<p>
+
+Kepala Sekolah
+
+</p>
+
+<p>
+
+NIY : 2006.04.0020
+
+</p>
+
+</div>
+
+</div>
+
+</body>
+
+</html>
